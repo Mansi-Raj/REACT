3f026a3c4375751bdc39dbd35c9f5aa81cc11192
MANSI RAJ <157103259+Mansi-Raj@users.noreply.github.com>
2026-01-16 17:47:55 +0530

    lesson 6

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { Routes, Route } from 'react-router';
+import { Orders } from './pages/Orders';
+import { Tracking } from './pages/Tracking';
 import './App.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route path="orders" element={<Orders />}/>
+      <Route path="tracking" element={<Tracking />} />
     </Routes>
   )
 }
