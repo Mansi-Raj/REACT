98c34165ee86eb483d2f132d32d243a9a708dc4d
MANSI RAJ <157103259+Mansi-Raj@users.noreply.github.com>
2026-01-16 13:30:45 +0530

    lesson 6 checkout page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,5 @@
 import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
 import { Routes, Route } from 'react-router';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="Checkout" element={<CheckoutPage />}></Route>
     </Routes>
   )
 }
